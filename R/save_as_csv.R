0eddfedf98814c4ce58f467a59566a400dbb560c
alexchouraki <alexchouraki@me.com>
2017-10-16 14:05:05 +0200

    with save as csv

---
+++ R/save_as_csv.R
@@ -0,0 +1,22 @@
+#' Save as CSV
+#' This function saves the dataframe as a csv
+#' @param dframe the dataframe you want to save as .csv
+#' @param filename the name and path you want to for your new .csv file
+#' @param row.names false
+#' @return
+#' @export
+#' @import
+#' dplyr
+#' assertthat
+#' @examples
+#'
+
+save_as_csv <- function(dframe, filename, row.names = FALSE, ...){
+  assert_that(is.data.frame(dframe))
+  assert_that(not_empty(dframe))
+  assert_that(is.dir(dirname(filename)))
+  assert_that(is.writeable(dirname(filename)))
+  assert_that(has_extension(filename,"csv"))
+  write.csv2(x = dframe, file = filename, row.names = row.names, ...)
+  invisible(normalizePath(filename))
+}
